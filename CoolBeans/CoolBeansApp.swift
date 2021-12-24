d3bd8fd26128e371709a8c613e3793b5efeea6f5
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-24 16:29:36 +0800

    Initial Commit

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Giovanni Gaffé on 2021/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
