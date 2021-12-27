7154e068b1cfca314667bd142c3ef2df50ebe773
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-27 13:57:49 +0800

    define an history where we save data

---
+++ CoolBeans/Model/History.swift
@@ -0,0 +1,12 @@
+//
+//  History.swift
+//  CoolBeans
+//
+//  Created by Giovanni Gaffé on 2021/12/27.
+//
+
+import Foundation
+
+class History: ObservableObject {
+    @Published var serving: [Serving]
+}
